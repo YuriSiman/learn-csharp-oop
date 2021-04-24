53043dc09563fe58925c725f885f793ed11b061a
Yuri Siman <yurisimannogueira@gmail.com>
2021-04-24 15:00:19 -0300

    Criando o projeto

---
+++ src/1 - Lógica de Programação/SaidaDeDados/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SaidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
