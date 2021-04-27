e48ab9f350d8ba48cc29321b4eb981e5dbe5b1e6
Yuri Siman <yurisimannogueira@gmail.com>
2021-04-27 08:57:48 -0300

    Refatorando o projeto

---
+++ src/1 - Lógica de Programação/2 - Operadores de Atribuição/OperadoresAtribuicao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OperadoresAtribuicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 0;
+            b += a++;
+
+            Console.WriteLine($"a - {a}\nb - {b}");
+        }
+    }
+}
---
+++ src/1 - Lógica de Programação/3 - Casting/Casting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            double b = 2;
+
+            int result = a / (int)b;
+            Console.WriteLine(result);
+        }
+    }
+}
