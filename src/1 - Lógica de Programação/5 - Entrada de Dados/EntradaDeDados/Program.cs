815d453ecba98bd9ba0623b9b43f149efc0c5f7d
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-08 18:16:03 -0300

    Modificando projeto Entrada de Dados

--- src/1 - Lógica de Programação/5 - Entrada de Dados/EntradaDeDados/Program.cs
+++ src/1 - Lógica de Programação/5 - Entrada de Dados/EntradaDeDados/Program.cs
@@ -6,16 +6,19 @@ namespace EntradaDeDados
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Faça um programa em console que receba do usuário o texto 'Banana Maçã Uva Morango' e imprima cada palavra do texto em uma linha separada");
+            Console.WriteLine("Faça um programa em console que receba do usuário um texto (uma linha)" +
+                " o primeiro nome, a idade, o salário e a primeira letra do nome, exemplo: 'Fulano 23 1000 F' " +
+                "e imprima cada palavra do texto em uma linha separada sendo nome do tipo string, idade do tipo int, " +
+                "salário do tipo double e primeira letra do nome do tipo char'");
 
             string[] vetor = Console.ReadLine().Split(' ');
 
-            string v1 = vetor[0];
-            string v2 = vetor[1];
-            string v3 = vetor[2];
-            string v4 = vetor[3];
+            string nome = vetor[0];
+            int idade = int.Parse(vetor[1]);
+            double salario = double.Parse(vetor[2]);
+            char letra = char.Parse(vetor[3]);
 
-            Console.WriteLine($"\nPrimeira palavra: {v1}\nSegunda palavra: {v2}\nTerceira palavra: {v3}\nQuarta palavra: {v4}");
+            Console.WriteLine($"\nPrimeiro nome: {nome}\nIdade: {idade}\nSalário: R$ {salario.ToString("F2")}\nPrimeira letra do nome: {letra}");
         }
     }
 }
