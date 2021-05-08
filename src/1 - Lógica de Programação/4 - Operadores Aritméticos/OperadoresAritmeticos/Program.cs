96f859fb772d753ef92701f5c5cc7c895f7df6be
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-08 12:35:49 -0300

    Criando o projeto Operações Aritméticas

---
+++ src/1 - Lógica de Programação/4 - Operadores Aritméticos/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OperadoresAritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva a fórmula de Báskara e execute em console mostrando os resultados de ambas as raízes. O projeto tem como finalidade o estudo de operadores aritméticos.");
+
+            double a = 1, b = -3, c = -4;
+
+            double delta = Math.Pow(b, 2) - (4 * a * c);
+            double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+            Console.WriteLine($"x1 = {x1}\nx2 = {x2}");
+        }
+    }
+}
