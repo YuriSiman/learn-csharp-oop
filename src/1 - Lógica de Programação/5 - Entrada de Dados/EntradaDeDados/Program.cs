021f1bb77f4a7a8f5e3a3d6d44529f34578f62e4
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-08 14:07:14 -0300

    Criando projeto Entrada de Dados

---
+++ src/1 - Lógica de Programação/5 - Entrada de Dados/EntradaDeDados/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faça um programa em console que receba do usuário o texto 'Banana Maçã Uva Morango' e imprima cada palavra do texto em uma linha separada");
+
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            string v1 = vetor[0];
+            string v2 = vetor[1];
+            string v3 = vetor[2];
+            string v4 = vetor[3];
+
+            Console.WriteLine($"\nPrimeira palavra: {v1}\nSegunda palavra: {v2}\nTerceira palavra: {v3}\nQuarta palavra: {v4}");
+        }
+    }
+}
