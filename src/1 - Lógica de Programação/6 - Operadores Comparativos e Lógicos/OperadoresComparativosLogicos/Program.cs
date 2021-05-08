b145a50812fb1d39982cc0e9ee54b0f9594dbabc
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-08 19:10:37 -0300

    Criando projeto Operadores Comparativos e Lógicos

---
+++ src/1 - Lógica de Programação/6 - Operadores Comparativos e Lógicos/OperadoresComparativosLogicos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OperadoresComparativosLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool comparador1 = 2 > 3 || 4 != 5;
+            bool comparador2 = !(2 > 3) && 4 != 5;
+
+            Console.WriteLine($"Comparador1: {comparador1}\nComparador2: {comparador2}");
+
+            bool comparador3 = 10 < 5;
+            bool comparador4 = comparador1 || comparador2 && comparador3;
+
+            Console.WriteLine($"Comparador4: {comparador4}");
+        }
+    }
+}
