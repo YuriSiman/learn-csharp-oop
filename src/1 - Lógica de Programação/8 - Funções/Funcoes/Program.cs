3f57819da937b4896fc194f02fbc36a3ff6204ec
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-15 15:54:40 -0300

    Criando o projeto Funções

---
+++ src/1 - Lógica de Programação/8 - Funções/Funcoes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite três números:");
+
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            int resultado = Maior(n1,n2,n3);
+            Console.WriteLine($"O maior número é o {resultado}");
+        }
+
+        private static int Maior(int a, int b, int c)
+        {
+            if(a > b && a > c)
+            {
+                return a;
+            }
+            else if (b > c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        } 
+    }
+}
