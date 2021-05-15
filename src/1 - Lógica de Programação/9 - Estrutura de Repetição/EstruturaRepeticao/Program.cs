0625a2025d025717cbaf23d46e3bef3fd6e8cd0f
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-15 17:15:51 -0300

    Criando o projeto Estrutura de Repetição

---
+++ src/1 - Lógica de Programação/9 - Estrutura de Repetição/EstruturaRepeticao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EstruturaRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero = 1;
+
+            while (numero > 0)
+            {
+                Console.WriteLine("Digite um número:");
+                numero = int.Parse(Console.ReadLine());
+
+                if(numero < 0)
+                {
+                    Console.WriteLine("Número negativo!");
+                }
+                else
+                {
+                    double raizQuadrada = Math.Sqrt(numero);
+                    Console.WriteLine(raizQuadrada.ToString("F3"));
+                }
+            }
+        }
+    }
+}
