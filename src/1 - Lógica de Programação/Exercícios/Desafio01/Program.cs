242b7dd3e52fb4c52f96844298bb0387297092de
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-21 07:19:23 -0300

    Desafio 01

---
+++ src/1 - Lógica de Programação/Exercícios/Desafio01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Desafio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe dois valores inteiros\nPrimeiro Valor: ");
+            int primeiroValor = int.Parse(Console.ReadLine());
+            Console.Write("Segundo Valor: ");
+            int segundoValor = int.Parse(Console.ReadLine());
+
+            int soma = primeiroValor + segundoValor;
+
+            Console.WriteLine($"SOMA = {soma}");
+        }
+    }
+}
---
+++ src/1 - Lógica de Programação/Exercícios/Desafio02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Desafio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inform o diâmetro de um círculo: ");
+            double diametro = double.Parse(Console.ReadLine());
+
+            double raio = diametro / 2;
+            double pi = 3.14159;
+            double area = pi * (Math.Pow(raio, 2));
+
+            Console.WriteLine($"A = {area}");
+        }
+    }
+}
