0e356578a15bb3c9ffddadc7ae88dc04d3d3a357
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-21 07:38:16 -0300

    Desafio 03

---
+++ src/1 - Lógica de Programação/Exercícios/Desafio03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Desafio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe quatro valores inteiros");
+            Console.Write("A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("B: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("C: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.Write("D: ");
+            int d = int.Parse(Console.ReadLine());
+
+            var diferenca = (a * b) - (c * d);
+            Console.WriteLine($"Diferença = {diferenca}");
+        }
+    }
+}
