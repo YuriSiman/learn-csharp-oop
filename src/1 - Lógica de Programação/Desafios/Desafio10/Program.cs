3c76be6ad22433f628ddffd663ab3eac8bb494d3
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-22 16:57:52 -0300

    Desafio 10

---
+++ src/1 - Lógica de Programação/Desafios/Desafio10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Desafio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a hora inicial e a hora final do jogo");
+
+            Console.Write("Hora Inicial: ");
+            int horaInicial = int.Parse(Console.ReadLine());
+            Console.Write("Hora Final: ");
+            int horaFinal = int.Parse(Console.ReadLine());
+
+            int duracao = 0;
+
+            if(horaInicial > horaFinal)
+            {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+            else if(horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            }
+            else
+            {
+                duracao = 24;
+            }
+
+            if (duracao > 24)
+            {
+                Console.WriteLine("O jogo durou 24 horas");
+            }
+            else if(duracao < 1)
+            {
+                Console.WriteLine("O jogo durou 1 hora");
+            }
+            else
+            {
+                Console.WriteLine($"O jogo durou {duracao} horas");
+            }
+        }
+    }
+}
