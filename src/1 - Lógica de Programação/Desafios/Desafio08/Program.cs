7710f9bd4921ee7daba2ffabe5ffa232391fd990
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-22 13:02:45 -0300

    Desafio 08

---
+++ src/1 - Lógica de Programação/Desafios/Desafio08/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Desafio08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if(numero % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("ÍMPAR");
+            }
+        }
+    }
+}
