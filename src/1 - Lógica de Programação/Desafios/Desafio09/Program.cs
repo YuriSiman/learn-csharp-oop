02aeabfd27aa5f5618659090647b214d146d6659
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-22 16:17:10 -0300

    Desafio 09

---
+++ src/1 - Lógica de Programação/Desafios/Desafio09/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Desafio09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois valores inteiros");
+            Console.Write("A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("B: ");
+            int b = int.Parse(Console.ReadLine());
+
+            if(a == 0 || b == 0)
+            {
+                Console.WriteLine("São Múltiplos");
+            }
+            else
+            {
+                if (a > b)
+                {
+                    int divisao = a / b;
+                    if (divisao * b == a)
+                    {
+                        Console.WriteLine("São Múltiplos");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Não são Múltiplos");
+                    }
+                }
+                else
+                {
+                    int divisao = b / a;
+                    if (divisao * a == b)
+                    {
+                        Console.WriteLine("São Múltiplos");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Não são Múltiplos");
+                    }
+                }
+            }
+        }
+    }
+}
