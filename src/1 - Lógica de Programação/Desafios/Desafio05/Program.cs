86c43b8d6b1710cde7bc13dfc8f2b09e5d4e542b
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-22 11:41:35 -0300

    Desafio 05

---
+++ src/1 - Lógica de Programação/Desafios/Desafio05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite as informações das peças");
+
+            Console.Write("Código da peça 1: ");
+            int codigo1 = int.Parse(Console.ReadLine());
+            Console.Write("Quantidade de peças 1: ");
+            int qtd1 = int.Parse(Console.ReadLine());
+            Console.Write("Valor de cada unidade da peça 1: ");
+            double valor1 = double.Parse(Console.ReadLine());
+            
+            Console.Write("Código da peça 2: ");
+            int codigo2 = int.Parse(Console.ReadLine());
+            Console.Write("Quantidade de peças 2: ");
+            int qtd2 = int.Parse(Console.ReadLine());
+            Console.Write("Valor de cada unidade da peça 2: ");
+            double valor2 = double.Parse(Console.ReadLine());
+
+            double reultado = (qtd1 * valor1) + (qtd2 * valor2);
+
+            Console.WriteLine($"Valor a pagar: R$ {reultado.ToString("F2")}");
+
+        }
+    }
+}
