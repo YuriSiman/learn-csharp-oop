c9af1ac71e95e10b6e4eac6c51bb13027a9a2214
Yuri Siman <yurisimannogueira@gmail.com>
2021-05-22 12:55:33 -0300

    Desafio 06

--- src/1 - Lógica de Programação/Desafios/Desafio06/Program.cs
+++ src/1 - Lógica de Programação/Desafios/Desafio06/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Desafio06
 {
@@ -6,7 +7,25 @@ namespace Desafio06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe três falores de ponto flutuante: ");
+            Console.Write("Valor A: ");
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Valor B: ");
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Valor C: ");
+            double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaTrianguloRetangulo = (a * c) / 2;
+            double areaCirculo = 3.14159 * Math.Pow(c, 2);
+            double areaTrapezio = ((a + b) / 2) * c;
+            double areaQuadrado = Math.Pow(b, 2);
+            double areaRetangulo = a * b;
+
+            Console.WriteLine($"TRIÂNGULO: {areaTrianguloRetangulo.ToString("F3", CultureInfo.InvariantCulture)}\n" +
+                $"CÍRCULO: {areaCirculo.ToString("F3", CultureInfo.InvariantCulture)}\n" +
+                $"TRAPÉZIO: {areaTrapezio.ToString("F3", CultureInfo.InvariantCulture)}\n" +
+                $"QUADRADO: {areaQuadrado.ToString("F3", CultureInfo.InvariantCulture)}\n" +
+                $"RETÂNGULO: {areaRetangulo.ToString("F3", CultureInfo.InvariantCulture)}");
         }
     }
 }
