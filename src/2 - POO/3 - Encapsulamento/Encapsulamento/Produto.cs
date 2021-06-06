c2c17ad42444e369e941dfd688e2b9dfd0ebfd11
Yuri Siman <yurisimannogueira@gmail.com>
2021-06-06 15:34:25 -0300

    Projeto Encapsulamento

---
+++ src/2 - POO/3 - Encapsulamento/Encapsulamento/Produto.cs
@@ -0,0 +1,46 @@
+﻿namespace Encapsulamento
+{
+    class Produto
+    {
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto(string nome, double preco, int qtd)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = qtd;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set 
+            { 
+                if(!string.IsNullOrEmpty(value))
+                    _nome = value; 
+            }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProdutos(int qtd)
+        {
+            Quantidade += qtd;
+        }
+
+        public void RemoverProdutos(int qtd)
+        {
+            Quantidade -= qtd;
+        }
+
+        public override string ToString()
+        {
+            return $"{_nome}, R$ {Preco.ToString("F2")}, {Quantidade}, R$ {ValorTotalEmEstoque().ToString("F2")}";
+        }
+    }
+}
---
+++ src/2 - POO/3 - Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            Console.Write("Quantidade no Estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto produto = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine("Digite outro nome para o produto: ");
+            produto.Nome = Console.ReadLine();
+
+            Console.WriteLine($"Dados do Produto: {produto}");
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"Dados Atualizados: {produto}");
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"Dados Atualizados: {produto}");
+        }
+    }
+}
