e92e07835b2d063c9adafb5bdae133db839dca15
Yuri Siman <yurisimannogueira@gmail.com>
2021-06-06 09:08:53 -0300

    Projeto ClassesAtributosMetodos

---
+++ src/2 - POO/1 - Classes, Atributos e Métodos/ClassesAtributosMetodos/Produto.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassesAtributosMetodos
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProdutos(int qtd)
+        {
+            Quantidade += qtd;
+        }
+
+        public void RemoverProdutos(int qtd)
+        {
+            Quantidade -= qtd;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, R$ {Preco.ToString("F2")}, {Quantidade}, R$ {ValorTotalEmEstoque().ToString("F2")}";
+        }
+    }
+}
---
+++ src/2 - POO/1 - Classes, Atributos e Métodos/ClassesAtributosMetodos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassesAtributosMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            Console.Write("Quantidade no Estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do Produto: {produto}");
+            
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"Dados Atualizados: {produto}");
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"Dados Atualizados: {produto}");
+        }
+    }
+}
