5a7d91550fcfa1364a63a78532c0de207a3dabca
Yuri Siman <yurisimannogueira@gmail.com>
2021-12-14 22:56:10 -0300

    Implementando novos desafios - Desafio14 - Desafio15 - Desafio16

---
+++ src/1 - Lógica de Programação/Desafios/Desafio15/Program.cs
@@ -0,0 +1,11 @@
+﻿int senha = 0;
+
+while (senha != 2002)
+{
+    Console.Write("Digite a senha: ");
+    senha = int.Parse(Console.ReadLine());
+
+    if (senha != 2002) Console.WriteLine("Senha Incorreta!");
+}
+
+Console.WriteLine("Senha Correta!");
---
+++ src/1 - Lógica de Programação/Desafios/Desafio16/Program.cs
@@ -0,0 +1,39 @@
+﻿
+Console.Write("Coordenada X: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Coordenada Y: ");
+int y = int.Parse(Console.ReadLine());
+
+
+while(x != 0 && y != 0)
+{
+    if (x < 0)
+    {
+        if(y < 0)
+        {
+            Console.WriteLine("Terceiro");
+        }
+        else
+        {
+            Console.WriteLine("Segundo");
+        }
+    }
+    else
+    {
+        if (y < 0)
+        {
+            Console.WriteLine("Quarto");
+        }
+        else
+        {
+            Console.WriteLine("Primeiro");
+        }
+    }
+
+    Console.Write("Coordenada X: ");
+    x = int.Parse(Console.ReadLine());
+
+    Console.Write("Coordenada Y: ");
+    y = int.Parse(Console.ReadLine());
+}
