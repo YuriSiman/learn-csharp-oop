9b29ae3ef6cc47624cf40f6d2ec5447c067e25db
Yuri Siman <yurisimannogueira@gmail.com>
2021-12-15 19:42:06 -0300

    Desafio20

---
+++ src/1 - Lógica de Programação/Desafios/Desafio19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Informe a quantidade de valores: ");
+int qtd = int.Parse(Console.ReadLine());
+int dentro = 0, fora = 0;
+
+for (int i = 1; i <= qtd; i++)
+{
+    Console.Write($"{i}° valor: ");
+    int valor = int.Parse(Console.ReadLine());
+
+    if(valor >= 10 && valor <= 20)
+    {
+        dentro++;
+    }
+    else
+    {
+        fora++;
+    }
+}
+
+Console.WriteLine($"{dentro} in\n{fora} out");
+
