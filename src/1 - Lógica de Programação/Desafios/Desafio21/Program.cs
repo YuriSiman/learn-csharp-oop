b75005494a3b1820531ce9d4ccff67a9822749bb
Yuri Siman <yurisimannogueira@gmail.com>
2021-12-15 20:24:47 -0300

    Desafio21

---
+++ src/1 - Lógica de Programação/Desafios/Desafio21/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Informe a quantidade de valores: ");
+int qtd = int.Parse(Console.ReadLine());
+double divisao;
+
+for (int i = 0; i < qtd; i++)
+{
+    Console.Write("Valores: ");
+    string[] valores = Console.ReadLine().Split(' ');
+
+    double valor1 = double.Parse(valores[0]);
+    double valor2 = double.Parse(valores[1]);
+
+    if (valor2 == 0)
+    {
+        Console.WriteLine("Divisão Impossível!");
+    }
+    else
+    {
+        divisao = valor1 / valor2;
+        Console.WriteLine($"{divisao:F1}");
+    }
+}
