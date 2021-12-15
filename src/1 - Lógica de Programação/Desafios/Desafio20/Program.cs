2c470191fa1e4a235f790fe4468e21241f609b28
Yuri Siman <yurisimannogueira@gmail.com>
2021-12-15 19:58:07 -0300

    Desafio20

---
+++ src/1 - Lógica de Programação/Desafios/Desafio20/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Informe a quantidade de casos de teste: ");
+int qtd = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < qtd; i++)
+{
+    Console.Write("Valores: ");
+    string[] valores = Console.ReadLine().Split(' ');
+    double valor1 = double.Parse(valores[0]);
+    double valor2 = double.Parse(valores[1]);
+    double valor3 = double.Parse(valores[2]);
+
+    double media = (valor1 * 2.0 + valor2 * 3.0 + valor3 * 5.0) / 10.0;
+    Console.WriteLine($"{media:F1}");
+}
+
+
