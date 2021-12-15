da1c280ee01b84453fd36236d49f9205b3dbd925
Yuri Siman <yurisimannogueira@gmail.com>
2021-12-15 07:45:42 -0300

    Desafio 17 - Desafio 18

---
+++ src/1 - Lógica de Programação/Desafios/Desafio17/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int combustivel = 0, alcool = 0, gasolina = 0, diesel = 0;
+
+while(combustivel != 4)
+{
+    Console.Write("Informe o Tipo de Combustível: ");
+    combustivel = int.Parse(Console.ReadLine());
+
+    if (combustivel < 1 || combustivel > 4)
+    {
+        Console.WriteLine($"Código [{combustivel}] Inválido! Informe um novo código.");
+    }
+    else if(combustivel == 1)
+    {
+        alcool++;
+    }
+    else if(combustivel == 2)
+    {
+        gasolina++;
+    }
+    else if(combustivel == 3)
+    {
+        diesel++;
+    }
+}
+
+Console.WriteLine($"\n\nMuito Obrigado!\nÁlcool: {alcool}\nGasolina: {gasolina}\nDiesel: {diesel}");
+
---
+++ src/1 - Lógica de Programação/Desafios/Desafio18/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Digite um valor inteiro de 1 a 1000: ");
+int x = int.Parse(Console.ReadLine());
+
+while(x < 1 || x > 1000)
+{
+    Console.Write("Valor Inválido!\nDigite um valor inteiro de 1 a 1000: ");
+    x = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < x; i++)
+{
+    if (!(i % 2 == 0)) Console.WriteLine($"Valor Ímpar: {i}"); 
+}
+
+if (!(x % 2 == 0)) Console.WriteLine($"Valor Ímpar: {x}");
