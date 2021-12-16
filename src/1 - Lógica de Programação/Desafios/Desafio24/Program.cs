d6a7277043368f0cef2c416ecfe25af0d655f30f
Yuri Siman <yurisimannogueira@gmail.com>
2021-12-16 07:29:46 -0300

    Desafio24

---
+++ src/1 - Lógica de Programação/Desafios/Desafio24/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int num = 0;
+
+while(num <= 0)
+{
+    Console.Write("\nInforme um número inteiro positivo: ");
+    num = int.Parse(Console.ReadLine());
+
+    if (num <= 0) Console.WriteLine("O número não é um inteiro positivo!");
+}
+
+for (int i = 1; i <= num; i++)
+{
+    Console.WriteLine($"{i} {Math.Pow(i,2)} {Math.Pow(i, 3)}");
+}
+
