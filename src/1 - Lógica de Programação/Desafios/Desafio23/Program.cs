a29ddd72f005bb933be570fc3e69f54f4a3cfd17
Yuri Siman <yurisimannogueira@gmail.com>
2021-12-15 21:34:05 -0300

    Desafio23

---
+++ src/1 - Lógica de Programação/Desafios/Desafio23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Número: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    int resul = num / i;
+
+    if(resul * i == num)
+    {
+        Console.WriteLine(resul);
+    }
+}
