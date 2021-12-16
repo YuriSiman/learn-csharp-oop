faa0fef59acbafe0308629d86b5341db2f629dc6
Yuri Siman <yurisimannogueira@gmail.com>
2021-12-15 21:17:58 -0300

    Desafio22

---
+++ src/1 - Lógica de Programação/Desafios/Desafio22/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Número: ");
+int num = int.Parse(Console.ReadLine());
+
+int fatorial = 1;
+
+for (int i = 1; i <= num; i++)
+{
+    fatorial = fatorial * i;
+}
+
+Console.WriteLine(fatorial);
