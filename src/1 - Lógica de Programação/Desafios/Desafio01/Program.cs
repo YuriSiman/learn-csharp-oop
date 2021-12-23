8532e06384e7566e282ae5725294f59e39b8c9fa
Yuri Siman <yurisimannogueira@gmail.com>
2021-12-23 20:02:05 -0300

    Refatorando Desafio01

--- src/1 - Lógica de Programação/Desafios/Desafio01/Program.cs
+++ src/1 - Lógica de Programação/Desafios/Desafio01/Program.cs
@@ -6,14 +6,23 @@ namespace Desafio01
     {
         static void Main(string[] args)
         {
-            Console.Write("Informe dois valores inteiros\nPrimeiro Valor: ");
-            int primeiroValor = int.Parse(Console.ReadLine());
-            Console.Write("Segundo Valor: ");
-            int segundoValor = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            double aX = double.Parse(Console.ReadLine());
+            double bX = double.Parse(Console.ReadLine());
+            double cX = double.Parse(Console.ReadLine());
 
-            int soma = primeiroValor + segundoValor;
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            double aY = double.Parse(Console.ReadLine());
+            double bY = double.Parse(Console.ReadLine());
+            double cY = double.Parse(Console.ReadLine());
 
-            Console.WriteLine($"SOMA = {soma}");
+            double pX = (aX + bX + cX) / 2;
+            double pY = (aY + bY + cY) / 2;
+            double areaX = Math.Sqrt(pX * (pX - aX) * (pX - bX) * (pX - cX));
+            double areaY = Math.Sqrt(pY * (pY - aY) * (pY - bY) * (pY - cY));
+            char maiorArea = areaX > areaY ? 'X' : 'Y';
+
+            Console.WriteLine($"Área de X: {areaX:F4}\nÁrea de X: {areaY:F4}\nMaior Área: {maiorArea}");
         }
     }
 }
