19c90ad154a97c968708a53b5e0cf4e4be08fca3
Yuri Siman <yurisimannogueira@gmail.com>
2022-01-04 22:16:29 -0300

    Desafio03 POO

---
+++ src/2 - POO/Desafios/Desafio03/Desafio03/Aluno.cs
@@ -0,0 +1,32 @@
+﻿namespace Desafio03
+{
+    internal class Aluno
+    {
+        public string Nome { get; set; }
+        public double Nota { get; set; }
+        public bool Situacao { get; set; }
+
+        public void NotaFinal(double nota)
+        {
+            Nota += nota;
+        }
+
+        public bool Status()
+        {
+            Situacao = Nota >= 60 ? true : false;
+            return Situacao;
+        }
+
+        public double NotaRestante()
+        {
+            if (Situacao) return 0.0;
+
+            return 60 - Nota;
+        }
+
+        public override string ToString()
+        {
+            return $"\nNota Final: {Nota:F2}"; 
+        }
+    }
+}
---
+++ src/2 - POO/Desafios/Desafio03/Desafio03/Program.cs
@@ -0,0 +1,24 @@
+﻿using Desafio03;
+
+Aluno aluno = new Aluno();
+
+Console.Write("Informe o nome do aluno: ");
+aluno.Nome = Console.ReadLine();
+
+for (int i = 1; i <= 3; i++)
+{
+    Console.Write($"\nInforme a {i}ª nota: ");
+    double nota = double.Parse(Console.ReadLine());
+
+    while ((i == 1 && nota > 30) || ((i == 2 || i == 3) && nota > 35))
+    {
+        Console.Write($"\n\nNota incorreta para o {i}º trimestre\nInforme a {i}ª nota: ");
+        nota = double.Parse(Console.ReadLine());
+    }
+        
+    aluno.NotaFinal(nota);
+}
+
+string situacaoFinal = aluno.Status() ? "APROVADO" : "REPROVADO";
+
+Console.WriteLine($"{aluno}\nSituação: {situacaoFinal}\nNota Restante: {aluno.NotaRestante():F2}");
