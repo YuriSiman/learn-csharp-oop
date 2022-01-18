6ed7b11ec6babe209700f2ddc457faff0bc5db58
Yuri Siman <yurisimannogueira@gmail.com>
2022-01-17 22:04:57 -0300

    Vetores

---
+++ src/1 - Lógica de Programação/Vetores/Vetores/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Informe uma quantidade de pessoas: ");
+int qtdPessoas = int.Parse(Console.ReadLine());
+
+double[] altura = new double[qtdPessoas];
+double somaAltura = 0;
+
+for (int i = 1; i <= qtdPessoas; i++)
+{
+    Console.Write($"Altura da {i}ª pessoa: ");
+    altura[i - 1] = double.Parse(Console.ReadLine());
+    somaAltura += altura[i - 1];
+}
+
+double media = somaAltura / qtdPessoas;
+Console.WriteLine($"Altura média: {media:F2}");
+
+
