ed305a9feef7a16031df9a2a473184d84a8f4ed0
Yuri Siman <yurisimannogueira@gmail.com>
2022-01-20 23:14:35 -0300

    Boxing e Unboxing

---
+++ src/3 - Comportamento de Memória/BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,9 @@
+﻿// Boxing é o processo de conversão de um objeto tipo valor para um objeto tipo referência compatível
+
+int x = 20;
+Object obj = x;
+
+// Unboxing é o processo de conversão de um objeto tipo referência para um objeto tipo valor compatível
+int y = 20;
+Object obj2 = y;
+int z = (int) obj;
--- src/3 - Comportamento de Memória/RefOut/RefOut/Calculadora.cs
+++ src/3 - Comportamento de Memória/RefOut/RefOut/Calculadora.cs
@@ -2,12 +2,13 @@
 {
     internal class Calculadora
     {
-        // Fazendo o parâmetro ser uma referência para a variável original
+        // Fazendo o parâmetro ser uma referência para a variável original, exigindo que a variável original seja iniciada
         public static void TriplicarRef(ref int x)
         {
             x = x * 3;
         }
 
+        // Fazendo o parâmetro ser uma referência para a variável original, sem exigir que a variável original seja iniciada
         public static void TriplicarOut(int origem, out int resultado)
         {
             resultado = origem * 3;
