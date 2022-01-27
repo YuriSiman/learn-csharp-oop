b44c9509814e75201a1c5759f6b439f63015e59b
Yuri Siman <yurisimannogueira@gmail.com>
2022-01-26 22:34:31 -0300

    Construtores Privados

---
+++ src/2 - POO/ConstrutoresSobrecarga/Log.cs
@@ -0,0 +1,21 @@
+﻿namespace ConstrutoresSobrecarga
+{
+    internal class Log
+    {
+        private static Log _log;
+        public string PropriedadeLog { get; set; }
+
+        // Definindo um construtor privado para bloquear qualquer tentativa de instância da classe Log
+        private Log() { }
+
+        // Método criado para quando for necessário usar a classe Log, trabalhando com a mesma instância durante toda a aplicação
+        public static Log GetInstance()
+        {
+            if(_log == null)
+            {
+                _log = new Log();
+            }
+            return _log;
+        }
+    }
+}
--- src/2 - POO/ConstrutoresSobrecarga/Program.cs
+++ src/2 - POO/ConstrutoresSobrecarga/Program.cs
@@ -28,15 +28,32 @@ namespace ConstrutoresSobrecarga
 
             Console.WriteLine($"Dados do Produto: {produto}");
 
-            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            Console.Write("Digite a quantidade de produtos a ser adicionada ao estoque: ");
             produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
 
             Console.WriteLine($"Dados Atualizados: {produto}");
 
-            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            Console.Write("Digite a quantidade de produtos a ser removida do estoque: ");
             produto.RemoverProdutos(int.Parse(Console.ReadLine()));
 
             Console.WriteLine($"Dados Atualizados: {produto}");
+
+
+            // Trabalhando com Construtor Privado
+
+            // Não é possível criar uma instância de um construtor privado.
+            // Suponhamos que nós queremos utilizar o Log para conectar em algum serviço externo (ou uma conexão com um banco de dados) e registrar os eventos que acontecem na sua aplicação. Dependendo da complexidade do sistema, será necessário
+            // instanciar a classe Log várias vezes em métodos específicos para registrar os eventos, e isso é prejudicial para a sua aplicação. Então, nesse casos em que é necessário ter uma classe que passa por vários lugares, podemos utilizar 
+            // um padrão de projeto chamado Sigleton, pois ele permite uma única instância da sua classe e ele reaproveita essa instância durante todo o ciclo de vida da sua aplicação. No nosso exemplo, chamaremos a Log em vários lugares, mas ele 
+            // sera o mesmo objeto em todos os lugares que for chamado.
+
+            Log log = Log.GetInstance();
+
+            log.PropriedadeLog = "\n\nTrabalhando com Construtor Privado - Teste Instância";
+
+            Log log2 = Log.GetInstance();
+
+            Console.WriteLine(log2.PropriedadeLog);
         }
     }
 }
