44e1852d601400a72574c76dd90cd5c86ce2ab52
Yuri Siman <yurisimannogueira@gmail.com>
2022-01-29 19:41:13 -0300

    Dictionaty

---
+++ src/07 - Collections/Dictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("MG", "Minas Gerais");
+estados.Add("BA", "Bahia");
+
+Console.WriteLine("\n>>>> Lista\n");
+ListarEstados(estados);
+
+Console.WriteLine("\n>>>> Atualizando o Estado de Minas Gerais\n");
+string chaveProcurada = "MG";
+
+if(ValidarChave(estados, chaveProcurada))
+{
+    Console.WriteLine($"Valor Original: {estados[chaveProcurada]}");
+    estados[chaveProcurada] = "MG - Atualização";
+    Console.WriteLine($"Valor Atualizado: {estados[chaveProcurada]}");
+}
+
+Console.WriteLine("\n>>>> Lista\n");
+ListarEstados(estados);
+
+Console.WriteLine("\n>>>> Removendo o Estado de Minas Gerais\n");
+
+if (ValidarChave(estados, chaveProcurada))
+{
+    estados.Remove(chaveProcurada);
+}
+
+ListarEstados(estados);
+
+void ListarEstados(Dictionary<string, string> estados)
+{
+    foreach (KeyValuePair<string, string> item in estados)
+    {
+        Console.WriteLine($"Chave: {item.Key}\nValor: {item.Value}");
+    }
+}
+
+bool ValidarChave(Dictionary<string, string> estados, string chave)
+{
+    if (estados.TryGetValue(chave, out string valor)) return true;
+
+    Console.WriteLine($"Chave {chave} não existe!");
+    return false;
+}
